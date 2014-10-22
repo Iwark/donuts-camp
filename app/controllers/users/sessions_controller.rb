2b5c9f907a4453a13eca01aeec8e5a5c3aa4651c
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2014-10-22 15:07:44 +0900

    cancel

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,13 +8,13 @@ class Users::SessionsController < Devise::SessionsController
   end
  
   def destroy
-    super
-    entry = Entry.where(user_id: user.id, camp_id: 1).first
+    entry = Entry.where(user_id: current_user.id, camp_id: 1).first
     if entry
       if entry.status != "cancel"
         entry.status = "cancel"
         entry.save
       end
     end
+    super
   end
 end
